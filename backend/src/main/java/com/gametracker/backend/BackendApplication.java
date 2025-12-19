f6d9697ec70463a14ba6ac1d60a4452dc35114dc
Estella Perkons <estellaperkons@icloud.com>
2025-12-19 19:32:36 +0000

    made backend accessible to h2 console and to react frontend calls. started (really did just start) react work

--- backend/src/main/java/com/gametracker/backend/BackendApplication.java
+++ backend/src/main/java/com/gametracker/backend/BackendApplication.java
@@ -1,7 +1,21 @@
 package com.gametracker.backend;
 
+import com.gametracker.backend.dto.gameEntry.GameEntryCreationDTO;
+import com.gametracker.backend.dto.gameEntry.ReviewCreationDTO;
+import com.gametracker.backend.enums.Status;
+import com.gametracker.backend.model.Game;
+import com.gametracker.backend.model.GameEntry;
+import com.gametracker.backend.model.User;
+import com.gametracker.backend.repository.GameEntryRepository;
+import com.gametracker.backend.repository.GameRepository;
+import com.gametracker.backend.repository.UserRepository;
+import com.gametracker.backend.service.GameEntryService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +23,22 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository, GameRepository gameRepository, GameEntryService gameEntryService, GameEntryRepository gameEntryRepository) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                User user = userRepository.save(new User(null, "Estella Perkons"));
+                Game game = gameRepository.save(new Game(null, "Metal Gear Solid V: The Phantom Pain", LocalDate.now()));
+                GameEntryCreationDTO dto = new GameEntryCreationDTO(user.getId(), game.getId(), Status.PLAYING, "Quiet! Put some clothes on!");
+                GameEntry newGameEntry = new GameEntry(user, game, dto.status(), dto.notes());
+                gameEntryRepository.save(newGameEntry);
+                ReviewCreationDTO dto2 = new ReviewCreationDTO(null, "fuckin awesome", LocalDate.now(), 10, 10, 10, 8, 7, 7);
+                gameEntryService.createReview(newGameEntry.getId(), dto2);
+            } else {
+                System.out.println("Database already contains data. Skipping initial setup.");
+            }
+        };
+    }
 
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom';
+// imports for main page sections (sidebar, dashboard, library)
+import Dashboard from './views/Dashboard.jsx';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app-container" style={{ display: 'flex' }}>
+      {/*<Sidebar />*/}
+
+      <main style={{ flex: 1, padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {/*<Route path="/library" element={<Library />} />*/}
+        </Routes>
+      </main>
+    </div>
   )
 }
-
-export default App
